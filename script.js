f6788a9cd6670b1e8f7899fbb8da7f6fc3dc343d
youssefzeanalabden-boop <youssefzeanalabden@gmail.com>
2026-07-02 23:30:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Tiger X Website!");
+
+const btn = document.querySelector(".btn");
+
+if (btn) {
+  btn.addEventListener("click", function () {
+    alert("Welcome to Tiger X! 🔥");
+  });
+}
